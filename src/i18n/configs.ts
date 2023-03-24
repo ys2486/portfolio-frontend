4be109f3e7e16b11c2bfb869550729c6609b5cd9
ys2486 <yusaku19940728@yahoo.co.jp>
2023-03-24 16:25:32 +0900

    2023/03/28 初期言語日本語に修正

--- src/i18n/configs.ts
+++ src/i18n/configs.ts
@@ -18,7 +18,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'en',
+    lng: 'ja',
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
